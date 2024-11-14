e8fdf9aa2fd73fe3360d2b3a6cf61acceeb13f9b
Nusrat Ahmmed Maisha <nusratamaisha@gmail.com>
2024-11-14 21:47:26 +0600

    removed app

--- apartment_rental/urls.py
+++ apartment_rental/urls.py
@@ -3,13 +3,13 @@ from django.urls import path
 from . import settings
 from django.conf.urls.static import static
 from rental import views as prop_views
-from auth import views as auth_views
+#from auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', prop_views.home, name="home"),
-    path('login/', auth_views.login, name='login'),
-    path('register/', auth_views.register, name='register'),
+    #path('login/', auth_views.login, name='login'),
+    #path('register/', auth_views.register, name='register'),
     path('property/', prop_views.property, name='property'),
     path('add_property/', prop_views.add_property, name='add_property'),
     path('update_property/<str:id>',prop_views.update_property,name='update_property'),
